18e338ef17ff75f89fd928abbfff137d2b7a1db8
Bhavin-Kareliya <bhavin.kareliya2017@gmail.com>
2023-06-29 08:12:54 +0530

    Upto: working on action results

---
+++ PracticalTen/Common/JavascriptResult.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticalTen.Common
+{
+    public class JavascriptResult : ContentResult
+    {
+        public JavascriptResult(string value)
+        {
+            this.Content = value;
+            this.ContentType = "application/javascript";
+        }
+    }
+}
---
+++ PracticalTen/Controllers/TestTwoController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticalTen.Common;
+using System.Net.Mime;
+
+namespace PracticalTen.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class TestTwoController : Controller
+    {
+        private IWebHostEnvironment _env;
+        public TestTwoController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public FileContentResult GetFileContentResult()
+        {
+            var file = System.IO.File.ReadAllBytes(Path.Combine(_env.WebRootPath, "data/sample.pdf"));
+            return new FileContentResult(file, "application/pdf");
+        }
+
+        public ContentResult GetContentResult()
+        {
+            return Content("<h3>Hello World</h3>");
+        }
+
+        public EmptyResult GetEmptyResult()
+        {
+            return new EmptyResult();
+        }
+
+        public JavascriptResult GetJavascriptResult()
+        {
+            var jsScript = "alert('Hello World!');";
+            return new JavascriptResult(jsScript);
+        }
+
+        public JsonResult GetJsonResult()
+        {
+            var fileData = System.IO.File.ReadAllText(Path.Combine(_env.WebRootPath, "data/People.json"));
+            return Json(fileData);
+        }
+
+        public IActionResult GetPartialViewResult()
+        {
+            ViewBag.Message = "This content is loaded using partial view!";
+            return PartialView("_details");
+        }
+    }
+}
